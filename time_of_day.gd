6578023265769dfece5680dd4fef10ca2dd7a3a3
Hugo Locurcio <hugo.locurcio@hugo.pro>
2021-05-31 20:31:42 +0200

    Add point lights (enabled only at nighttime)
    - Allow smoother time of day changes using the slider.

--- time_of_day.gd
+++ time_of_day.gd
@@ -6,6 +6,7 @@ extends HBoxContainer
 
 @onready var sun := $"../Sun" as DirectionalLight3D
 @onready var moon := $"../Sun/Moon" as DirectionalLight3D
+@onready var point_lights := $"../PointLights" as Node3D
 @onready var time_label := $TimeLabel as Label
 @onready var slider := $HSlider as HSlider
 
@@ -16,12 +17,11 @@ func _ready() -> void:
 
 func _on_HSlider_value_changed(value: float) -> void:
 	sun.rotation_degrees.x = range_lerp(value, 0, 100, 130, -130)
-	if value <= 50:
-		sun.use_in_sky_only = true
-		moon.use_in_sky_only = false
-	else:
-		sun.use_in_sky_only = false
-		moon.use_in_sky_only = true
+	# Enable/disable sun/moon lights depending on time of day.
+	sun.use_in_sky_only = value <= 50
+	moon.use_in_sky_only = value >= 50
+	# Only enable lamps during night and early morning.
+	point_lights.visible = value <= 57.5
 
 	var minutes := int(range_lerp(value, 0, 100, 0, 900))
 	time_label.text = "%2d:%02d" % [minutes / 60, minutes % 60]
